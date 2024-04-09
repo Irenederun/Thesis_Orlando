8aa3470334ac24f4d5c9cd3654cfbd67cc089a46
RW_AI <103892538+RWAI524@users.noreply.github.com>
2024-04-09 18:08:04 -0400

    Inter2 changes

---
+++ Thesis_Orando/Assets/Scripts/ICScene/WordDragDetailsPlay3Inter.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WordDragDetailsPlay3Inter : WordDragDetails
+{
+public override void DragComplete(string wordd, string desNamee)
+    {
+        base.DragComplete(wordd, desNamee);
+
+        if (desNamee.Contains("1"))
+        {
+                    
+        wordPosition = 1;
+
+        string word2IA = WordDragManager.instance.FindConjugation(wordd, "2IA");
+            
+        text.text = word2IA;
+        RecordWords(wordPosition, word2IA);
+
+        }
+
+        else if (desNamee.Contains("2"))
+        {
+            wordPosition = 2;
+
+        string word2IB = WordDragManager.instance.FindConjugation(wordd, "2IB");
+            
+        text.text = word2IB;
+        RecordWords(wordPosition, word2IB);
+
+        WordDragManager.instance.responseDeterminant = WordDragManager.instance.FindConjugation(wordd, "2IC");
+        }
+
+    }
+
+    public void RecordWords(int pos, string text)
+    {
+        WordDragManager.instance.AddToSentence(text, pos);
+    }
+}
