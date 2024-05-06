75a77eb43fff093b34f46d60c97a06259c2349f8
RW_AI <103892538+RWAI524@users.noreply.github.com>
2024-05-05 21:00:11 -0400

    MENUUPUPDATE

--- Thesis_Orando/Assets/Scripts/UI/Menu/MenuLoadScene.cs
+++ Thesis_Orando/Assets/Scripts/UI/Menu/MenuLoadScene.cs
@@ -1,12 +1,32 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 
 public class MenuLoadScene : MonoBehaviour
 {
+    public Image fadeInImage; 
+    public float fadeInDuration = 1.0f; 
+
     public void StartGame()
     {
-        SceneManager.LoadSceneAsync(SceneManager.GetActiveScene().buildIndex + 1);    
+        StartCoroutine(StartGameWithFade());
+    }
+
+    IEnumerator StartGameWithFade()
+    {
+        float elapsedTime = 0;
+        while (elapsedTime < fadeInDuration)
+        {
+            float alpha = Mathf.Lerp(0, 1, elapsedTime / fadeInDuration);
+            fadeInImage.color = new Color(0, 0, 0, alpha);
+            elapsedTime += Time.deltaTime;
+            yield return null;
+        }
+
+        fadeInImage.color = new Color(0, 0, 0, 1);
+
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
 }
